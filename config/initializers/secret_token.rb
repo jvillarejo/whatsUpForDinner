6c895b45cb6cb8049b86318c30424bdfa3d05dca
jvillarejo <arzivian87@gmail.com>
2012-11-09 22:52:09 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatsfordinner::Application.config.secret_token = '596b967c306fc5f2f903ffee74423be1dd375b7e1bc8390fa2581f436fe7fe6044b582a52663b7e666f049983f13c1dca93568ae7d438758ad229bbe18298bbe'
